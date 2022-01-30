ae26d85e5260dc9f3bc9033b1b8cc493607216fc
Jose A. Jimenez <jajimenezcarm@gmail.com>
2022-01-30 21:00:25 +0100

    New Notebook dialog completed

--- src/app/new-notebook-dialog/new-notebook-dialog.component.ts
+++ src/app/new-notebook-dialog/new-notebook-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Notebook } from "../models/notebook.model";
+
+import { NotebookService } from "../services/notebook.service";
 
 @Component({
     selector: "app-new-notebook-dialog",
@@ -7,12 +10,31 @@ import { NgForm } from "@angular/forms";
     styleUrls: ["./new-notebook-dialog.component.css"]
 })
 export class NewNotebookDialogComponent implements OnInit {
-    constructor() { }
+    exists: boolean = false;
+
+    constructor(private notebookService: NotebookService) { }
 
     ngOnInit(): void {
     }
 
+    onNameInput(form: NgForm) {
+        const name: string = form.value.name;
+
+        this.notebookService.notebooks.subscribe({
+            next: (notebooks: Notebook[]) => {
+                this.exists = notebooks.find(
+                    (n: Notebook) => name.trim().toLowerCase() === n.name.trim().toLowerCase()
+                ) !== undefined
+            }
+        })
+    }
+
     onSubmit(form: NgForm) {
         if (!form.valid) return;
+        const name = form.value.name;
+
+        this.notebookService.create(name).subscribe({
+            error: (e: string) => console.error(e)
+        })
     }
 }
--- src/app/services/notebook.service.ts
+++ src/app/services/notebook.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject } from "rxjs";
+import { Observable, BehaviorSubject, throwError } from "rxjs";
+import { map, tap, catchError } from "rxjs/operators";
 
 import { environment } from "src/environments/environment";
 import { AuthService } from "src/app/services/auth.service";
@@ -20,6 +21,11 @@ interface NotebookListResponseData {
     ]
 }
 
+interface ResponseData {
+    message: string,
+    message_type: string
+}
+
 @Injectable({providedIn: "root"})
 export class NotebookService {
     // BehaviorSubject is a type of Subject, and therefore a type of Observable, which
@@ -32,22 +38,9 @@ export class NotebookService {
     constructor(private http: HttpClient, private authService: AuthService) {
         this.authService.authUser.subscribe(
             (u: AuthUser | null) => {
-                if (u) {
-                    // Get the notebook list data from the API. The Access Token is automatically added
-                    // as a header to to the request before sending it by the AuthInterceptor service.
-                    const url = environment.notelist_api_url + "/notebooks/notebooks";
-                    const request = this.http.get<NotebookListResponseData>(url);
-
-                    request.subscribe({
-                        next: (d: NotebookListResponseData) => this.notebooks.next(d.result.map(
-                            (x) => new Notebook(x.id, x.name, x.created_ts, x.last_modified_ts)
-                        )),
-                        error: (e: any) => this.authService.handleError(request, e)
-                    });
-                } else {
-                    // If the current value of "authUser" is "null", it means no user is logged in
-                    this.notebooks.next([]);
-                }
+                // If the current value of "authUser" is "null", it means no user is logged in
+                if (u) this.load()
+                else this.notebooks.next([]);
             }
         );
 
@@ -57,5 +50,39 @@ export class NotebookService {
                 else this.currentNotebook.next(n[0]);
             }
         )
+    }
+
+    load() {
+        // Get the notebook list data from the API. The Access Token is automatically added
+        // as a header to to the request before sending it by the AuthInterceptor service.
+        const url = environment.notelist_api_url + "/notebooks/notebooks";
+        const request = this.http.get<NotebookListResponseData>(url);
+
+        request.subscribe({
+            next: (d: NotebookListResponseData) => this.notebooks.next(d.result.map(
+                (x) => new Notebook(x.id, x.name, x.created_ts, x.last_modified_ts)
+            )),
+            error: (e: any) => this.authService.handleError(request, e)
+        });
+    }
+
+    create(name: string): Observable<void> {
+        const url = environment.notelist_api_url + "/notebooks/notebook";
+        const data = {name: name};
+
+        return this.http.post<ResponseData>(url, data).pipe(
+            map((d: ResponseData) => {}),
+            tap(() => this.load()),
+            catchError(errorResponse => {
+                // Replace the error response by its "error.message" property if it exists or
+                // by "Unknown error" otherwise.
+
+                if (!errorResponse.error || !errorResponse.error.message) {
+                    return throwError(() => new Error("Unknown error"));
+                }
+
+                return throwError(() => new Error(errorResponse.error.message));
+            })
+        );
     }
 }
