ceeb5491ae2c4777f925aef677cf9e7ed8de128e
Jose A. Jimenez <jajimenezcarm@gmail.com>
2022-03-11 02:31:32 +0100

    Note mobile view implemented

--- src/app/main/notebook/notebook.component.ts
+++ src/app/main/notebook/notebook.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, HostListener } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Subscription } from "rxjs";
 
 import { NotebookService } from "src/app/services/notebook.service";
 import { NoteService } from "src/app/services/note.service";
-import { NotePreview } from "src/app/models/note.model";
+import { NotePreview, Note } from "src/app/models/note.model";
 
 @Component({
     selector: "app-notebook",
@@ -12,18 +12,26 @@ import { NotePreview } from "src/app/models/note.model";
     styleUrls: ["./notebook.component.css"]
 })
 export class NotebookComponent implements OnInit, OnDestroy {
+    mobileWidth: boolean = false;
+    noteOpen: boolean = false;
     notes: NotePreview[] = [];
+
     notesSub: Subscription | undefined;
+    currentNoteSub: Subscription | undefined;
 
     constructor(
         private actRoute: ActivatedRoute,
         private notebookService: NotebookService,
-        private noteService: NoteService
+        private noteService: NoteService,
     ) {}
 
     ngOnInit(): void {
         this.notesSub = this.noteService.notes.subscribe({
             next: (notes: NotePreview[]) => this.notes = notes
+        });
+
+        this.currentNoteSub = this.noteService.currentNote.subscribe({
+            next: (note: Note | null) => this.noteOpen = note !== null
         });
 
         this.actRoute.params.subscribe({
@@ -32,10 +40,23 @@ export class NotebookComponent implements OnInit, OnDestroy {
                 if (notebookId) this.notebookService.setCurrentNotebook(notebookId).subscribe();
             }
         });
+
+        this.checkWindowWidth();
+    }
+
+    @HostListener("window:resize", ["$event"])
+    onWindowResize(event: Event) {
+        this.checkWindowWidth();
+    }
+
+    checkWindowWidth() {
+        this.mobileWidth = window.innerWidth < 576;
     }
 
     ngOnDestroy(): void {
         this.notesSub?.unsubscribe();
+        this.currentNoteSub?.unsubscribe();
+
         this.notebookService.setCurrentNotebook(null);
     }
 }
